0acdcc6c4079d7b6f900643195529f9bcf3da4aa
dabris <dabris16.x@gmail.com>
2018-11-11 21:47:14 -0500

    initializing

---
+++ src/Job.java
@@ -0,0 +1,13 @@
+
+public class Job {
+private String jobName;
+private int jobLength;//between 1 and 70
+private int currentJobLength;
+private int jobPriority;//between 1 and 40
+private int finalPriority;
+private long entryTime;
+private long endTime;
+private long waitTime;
+
+
+}
---
+++ src/LinkedListHeapPQ.java
@@ -0,0 +1,4 @@
+
+public class LinkedListHeapPQ {
+// First-Come-First-Served (FCFS)if currentPriority is the same for 2 jobs
+}
