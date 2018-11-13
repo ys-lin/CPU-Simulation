ba43185922730c1930412a5bd6b5aa0f7b74e1d4
dabris <dabris16.x@gmail.com>
2018-11-13 16:32:23 -0500

    unsorted insert removeMin methods

--- src/UnsortedListPQ.java
+++ src/UnsortedListPQ.java
@@ -1,16 +1,43 @@
 
 public class UnsortedListPQ{
-	int key;
-	Job value;
+	private long priorityChange=0;
+	private Job[] liQueue;
+	private int numElements=0;
+	
+	private boolean isHigherPriority(Job a, Job b) {
+		if(a.getJP()<b.getJP()) {
+			return true;
+		}
+		if(a.getJP()==b.getJP()) {
+			if(a.getTime()<b.getTime()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
 	
 public void insert(int key, Job value) {
-		
+		liQueue[++numElements]=liQueue[key];
+		liQueue[key]=value;
 	}
 
 	
-	public void removeMin() {
-		
-	}
+	public Job removeMin() {
+		int min=50;
+		int index=-1;
+		for(int i=1;i<=numElements;i++) {
+			if(liQueue[i].getJP()<min) {
+				min=liQueue[i].getJP();
+				index=i;
+			}
+		}
+		Job temp=liQueue[index];
+		liQueue[index]=liQueue[numElements];
+		numElements--;
+		return temp;
+		}
 
 	
 	public int min() {
